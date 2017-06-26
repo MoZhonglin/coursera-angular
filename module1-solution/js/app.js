4752f914ddb57fefa2ef0b5e8884d64ad7207325
MoZhonglin <frank.mo@live.com>
2017-06-26 16:18:53 +0800

    finish module 1 extra

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -8,6 +8,8 @@
 
     $scope.dishesList = "";  // user's input
     $scope.msg = "";  // feedback of user's input
+    $scope.borderStyle = "";
+    $scope.fontColor = "";
 
     // button click event handler function
     $scope.checkLunch = function() {
@@ -22,11 +24,17 @@
       }
 
       if (cnt == 0) {
-        $scope.msg = "Please enter data first"
+        $scope.msg = 'Please enter data first'
+        $scope.fontColor = 'red'
+        $scope.borderStyle = 'red_border'
       } else if (cnt <= 3) {
         $scope.msg = "Enjoy!"
+        $scope.fontColor = 'green'
+        $scope.borderStyle = 'green_border'
       } else {
         $scope.msg = "Too much!"
+        $scope.fontColor = 'green'
+        $scope.borderStyle = 'green_border'
       }
 
     }
